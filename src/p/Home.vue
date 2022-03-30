f4e41879bcfa169969722b4a6ab082ed9bfa3ab4
Zakaria <ztaraouat@hotmail.com>
2022-03-30 13:46:51 +0200

    master

--- src/p/Home.vue
+++ src/p/Home.vue
@@ -4,12 +4,22 @@
 
 <template>
     <div class="content-header">
-        <h1 class="big-title">Hello, there</h1>
+        <h1 class="big-title">Hello, there 👋</h1>
+    </div>
+    <div class="content-body">
+        <p class="m-paragraph">
+            Hello, i'm Zakaria TARAOUAT, an apprentice web developper. <br>
+            
+        </p>
     </div>
 </template>
 
 <script>
   export default {
-      
+      data: () => {
+        return {
+            
+        }
+      }      
   }
 </script>
