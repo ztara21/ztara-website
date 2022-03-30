96d1db416193f32b3272059887cff54662c785d4
Zakaria <ztaraouat@hotmail.com>
2022-03-30 12:39:59 +0200

    master

---
+++ src/p/Home.vue
@@ -0,0 +1,15 @@
+<style>
+  @import url('../style/Pages.css');
+</style>
+
+<template>
+    <div class="content-header">
+        <h1 class="big-title">Hello, there</h1>
+    </div>
+</template>
+
+<script>
+  export default {
+      
+  }
+</script>
