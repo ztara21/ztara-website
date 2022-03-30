c63c552d5e6726f0ce821b5e32f3aa17e319fd34
Zakaria <ztaraouat@hotmail.com>
2022-03-30 14:32:23 +0200

    master

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ## My personnal open-source website. 
 
-### If you want to suggest changes, contact me by email: mailto:ztaraouat@zohomail.com
+### If you want to suggest changes, contact me by email: ztaraouat@zohomail.com
--- src/p/Home.vue
+++ src/p/Home.vue
@@ -9,16 +9,41 @@
     <div class="content-body">
         <p class="m-paragraph">
             Hello, i'm Zakaria TARAOUAT, an apprentice web developper. <br>
-            
+            <br>
+            <strong>Connect with me: </strong>
         </p>
+        <div class="social-icons">
+            <a class="social-link" href="https://ztara.vercel.app">
+                <img class="social-img" src="../assets/img/globe-light.svg" alt="globe-link" />
+            </a>
+            <a class="social-link" href="https://github.com/ztara21">
+                <img src="../assets/img/github-light.svg" alt="github-link" />
+            </a>
+            <a class="social-link" href="https://twitter.com/ztara_21">
+                <img src="../assets/img/twitter-light.svg" alt="twitter-link" />
+            </a>
+            <a class="social-link" href="https://linkedin.com/in/ztara21">
+                <img src="../assets/img/linkedin-light.svg" alt="linkedin-link" />
+            </a>
+        </div>
+        <p class="m-paragraph">
+            <strong>Current languages & frameworks</strong>
+        </p>
+        <div class="languages-icons">
+            <img width="30px" class="lang-img" src="../assets/img/html5-original.svg" alt="html-link" />
+            <img width="30px" class="lang-img" src="../assets/img/css3-original.svg" alt="css-link" />
+            <img width="30px" class="lang-img" src="../assets/img/javascript-original.svg" alt="js-link" />
+            <img width="30px" class="lang-img" src="../assets/img/vuejs-original.svg" alt="vue-link" />
+            <img width="30px" class="lang-img" src="../assets/img/nodejs-original.svg" alt="node-link" />
+        </div>
     </div>
 </template>
 
 <script>
   export default {
       data: () => {
-        return {
-            
+        return {   
+
         }
       }      
   }
